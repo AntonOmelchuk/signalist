f28ead580514de9426b68627fe51450296ff3723
Anton Omelchuk <kino_kg@ukr.net>
2026-03-31 15:24:13 +0300

    add Sign Up page UI

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,16 @@
 "use client";
 import { useForm } from "react-hook-form";
 
+import { CountrySelectField } from "@/components/forms/CountrySelectField";
+import FooterLink from "@/components/forms/FooterLink";
 import InputField from "@/components/forms/InputField";
+import SelectField from "@/components/forms/SelectField";
 import { Button } from "@/components/ui/button";
+import {
+  INVESTMENT_GOALS,
+  PREFERRED_INDUSTRIES,
+  RISK_TOLERANCE_OPTIONS,
+} from "@/lib/constants";
 
 const SignUp = () => {
   const {
@@ -70,6 +78,40 @@ const SignUp = () => {
             minLength: { value: 8, message: "Min length is 8" },
           }}
         />
+        <CountrySelectField
+          name="country"
+          label="Country"
+          control={control}
+          error={errors.country}
+          required
+        />
+        <SelectField
+          name="investmentGoals"
+          label="Investment Goals"
+          error={errors.investmentGoals}
+          placeholder="Select your investment goal"
+          options={INVESTMENT_GOALS}
+          control={control}
+          required
+        />
+        <SelectField
+          name="riskTolerance"
+          label="Risk Tolerance"
+          error={errors.riskTolerance}
+          placeholder="Select your risk level"
+          options={RISK_TOLERANCE_OPTIONS}
+          control={control}
+          required
+        />
+        <SelectField
+          name="prefferedIndustry"
+          label="Preffered Industry"
+          error={errors.preferredIndustry}
+          placeholder="Select your preffered industry"
+          options={PREFERRED_INDUSTRIES}
+          control={control}
+          required
+        />
         <Button
           type="submit"
           disabled={isSubmitting}
@@ -77,6 +119,12 @@ const SignUp = () => {
         >
           {isSubmitting ? "Creating account" : "Start Your Investing Journey"}
         </Button>
+
+        <FooterLink
+          text="Already have an account"
+          linkText="Sign in"
+          href="/sign-in"
+        />
       </form>
     </>
   );
